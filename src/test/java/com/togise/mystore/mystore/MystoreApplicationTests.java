250a45e7cf525993c8d948fee36184f6cd6d86c5
Tolga Umut <tolga.umut@tr.com>
2019-03-16 08:12:41 -0500

    Initial project structure

---
+++ src/test/java/com/togise/mystore/mystore/MystoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.togise.mystore.mystore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MystoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
