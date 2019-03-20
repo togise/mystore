cfec75274195e8339c98ee7e3c0fc6ad63dcae2f
Tolga Umut <tolga.umut@tr.com>
2019-03-20 08:01:17 -0500

    added the local dynamodb

---
+++ product-repository/src/test/groovy/io/redskap/java/aws/dynamodb/example/local/testing/TestDynamoDbLocalStartStop.groovy
@@ -0,0 +1,53 @@
+package io.redskap.java.aws.dynamodb.example.local.testing
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
+import com.amazonaws.services.dynamodbv2.model.KeyType
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType
+import io.redskap.java.aws.dynamodb.example.local.testing.AwsDynamoDbLocalTestUtils
+import spock.lang.Specification
+
+/**
+ * Reference: https://github.com/aws-samples/aws-dynamodb-examples/blob/master/src/test/java/com/amazonaws/services/dynamodbv2/local/embedded/DynamoDBEmbeddedTest.java
+ */
+class TestDynamoDbLocalStartStop extends Specification {
+    def "test stop start" () {
+        AwsDynamoDbLocalTestUtils.initSqLite();
+        AmazonDynamoDB ddb = DynamoDBEmbedded.create().amazonDynamoDB()
+        String tableName = "Movies"
+        String hashKeyName = "film_id"
+
+        when:
+        CreateTableResult res = createTable(ddb, tableName, hashKeyName)
+
+        then:
+        res.getTableDescription().tableName == tableName
+        ddb.listTables().tableNames.size() == 1
+
+    }
+
+    private static CreateTableResult createTable(AmazonDynamoDB ddb, String tableName, String hashKeyName) {
+        List<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>()
+        attributeDefinitions.add(new AttributeDefinition(hashKeyName, ScalarAttributeType.S))
+
+        List<KeySchemaElement> ks = new ArrayList<KeySchemaElement>()
+        ks.add(new KeySchemaElement(hashKeyName, KeyType.HASH))
+
+        ProvisionedThroughput provisionedthroughput = new ProvisionedThroughput(1000L, 1000L)
+
+        CreateTableRequest request =
+                new CreateTableRequest()
+                        .withTableName(tableName)
+                        .withAttributeDefinitions(attributeDefinitions)
+                        .withKeySchema(ks)
+                        .withProvisionedThroughput(provisionedthroughput)
+
+        return ddb.createTable(request)
+    }
+
+}
